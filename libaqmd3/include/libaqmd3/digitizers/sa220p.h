93893c97b8f5adaa2a0f0eec7eea75ad575595be
Giberson, Cam <cameron.giberson@gmail.com>
2026-04-22 22:39:08 -0700

    add missed

--- libaqmd3/include/libaqmd3/digitizers/sa220p.h
+++ libaqmd3/include/libaqmd3/digitizers/sa220p.h
@@ -61,7 +61,7 @@ private:
 	}
 
 public:
-	SA220P(std::string device_handle, bool simulate_device) : Digitizer(device_handle, simulate_device, "SA220P")
+	SA220P(std::string device_handle, bool simulate_device) : Digitizer(device_handle, simulate_device, "SA220")
 	{
 		digitizer_info = get_digitizer_info();
 	}
@@ -84,18 +84,18 @@ public:
 };
 
 template <>
-struct DeviceConstants<SA220>
+struct DeviceConstants<SA220P>
 {
 	static DeviceConstantsMap get_device_constants_map()
 	{
 		auto device_constants = DeviceConstantsMap {
-			{DigitizerConfigurableType::ChannelValues, std::vector<std::string>{SA220::channel_1, SA220::channel_2}},
-			{DigitizerConfigurableType::SampleChannelValues, std::vector<std::string>{SA220::samples_channel_1, SA220::samples_channel_2}},
-			{DigitizerConfigurableType::MarkersChannelValues, std::vector<std::string>{SA220::markers_channel_1, SA220::markers_channel_2}},
-			{DigitizerConfigurableType::TriggerValues, std::vector<std::string>{SA220::trigger_internal_1, SA220::trigger_internal_2, SA220::trigger_external, SA220::trigger_software, SA220::trigger_self}},
-			{DigitizerConfigurableType::ControlIOValues, std::vector<std::string>{SA220::control_io_1, SA220::control_io_2, SA220::control_io_3}},
-			{DigitizerConfigurableType::FullScaleRangeValues, std::vector<double>{SA220::full_scale_range_0_5v, SA220::full_scale_range_2_5v}},
-			{DigitizerConfigurableType::SampleRateValues, std::vector<double>{SA220::sample_rate_1GS, SA220::sample_rate_2GS}}
+			{DigitizerConfigurableType::ChannelValues, std::vector<std::string>{SA220P::channel_1, SA220P::channel_2}},
+			{DigitizerConfigurableType::SampleChannelValues, std::vector<std::string>{SA220P::samples_channel_1, SA220P::samples_channel_2}},
+			{DigitizerConfigurableType::MarkersChannelValues, std::vector<std::string>{SA220P::markers_channel_1, SA220P::markers_channel_2}},
+			{DigitizerConfigurableType::TriggerValues, std::vector<std::string>{SA220P::trigger_internal_1, SA220P::trigger_internal_2, SA220P::trigger_external, SA220P::trigger_software, SA220P::trigger_self}},
+			{DigitizerConfigurableType::ControlIOValues, std::vector<std::string>{SA220P::control_io_1, SA220P::control_io_2, SA220P::control_io_3}},
+			{DigitizerConfigurableType::FullScaleRangeValues, std::vector<double>{SA220P::full_scale_range_0_5v, SA220P::full_scale_range_2_5v}},
+			{DigitizerConfigurableType::SampleRateValues, std::vector<double>{SA220P::sample_rate_1GS, SA220P::sample_rate_2GS}}
 		};
 
 		return device_constants;
