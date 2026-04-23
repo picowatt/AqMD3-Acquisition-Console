c18098dbb3d9e137521e3d094be506c3b8b7cdf5
Giberson, Cam <cameron.giberson@gmail.com>
2026-04-22 23:36:50 -0700

    add beginnings of cli

--- AqMD3_cli/src/aqmd3_cli.cpp
+++ AqMD3_cli/src/aqmd3_cli.cpp
@@ -1,6 +1,13 @@
+#include <libaqmd3/digitizers/sa220p.h>
+#include <libaqmd3/devicevalues/deviceconstants.h>
+#include <libaqmd3/devicevalues/digitizerconfigurabletype.h>
 #include <iostream>
 #include <string>
 #include <cxxopts.hpp>
+#include <string>
+#include <vector>
+#include <variant>
+#include <unordered_map>
 
 
 int main(int argc, char *argv[])
@@ -9,7 +16,6 @@ int main(int argc, char *argv[])
     {
         cxxopts::Options options("AqMD3_cli", "A CLI for controlling the digitizers");
 
-        // Define options
         options.add_options()
             ("h,help", "Print help")
             ("d,digitizer", "The digitizer", cxxopts::value<std::string>()->default_value("SA220P"));
@@ -24,8 +30,19 @@ int main(int argc, char *argv[])
 
         std::string name = result["digitizer"].as<std::string>();
 
+        auto digi_consts = aqmd3::get_device_constants_map<aqmd3::SA220P>();
 
-
+        // https://en.cppreference.com/cpp/utility/variant/visit2
+        for (const auto &[key, values] : digi_consts)
+        {
+            std::visit([](const auto& vector)
+            {
+                for (const auto& elem : vector)
+                {
+                    std::cout << elem << std::endl;
+                }
+            }, values);
+        }
     }
     catch (const cxxopts::exceptions::exception &e)
     {
